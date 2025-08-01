2f06e452bd690b375e6c1a85575c037a5c382ad1
Even Zhou <even.y.zhou@outlook.com>
2025-08-01 20:46:32 +0800

    try fix w8a8

--- test/srt/test_ascend_mla_w8a8int8.py
+++ test/srt/test_ascend_mla_w8a8int8.py
@@ -31,7 +31,6 @@ class TestAscendMlaW8A8Int8(CustomTestCase):
         cls.url = urlparse(DEFAULT_URL_FOR_TEST)
         cls.common_args = [
             "--trust-remote-code",
-            "--disable-cuda-graph",
             "--mem-fraction-static",
             0.8,
             "--attention-backend",
