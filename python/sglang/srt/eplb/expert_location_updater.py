f48391208d93ec987a6fcbd0efb030c912ce945f
qmkakaxi <wjh1594260677@qq.com>
2025-08-04 20:06:56 +0800

    fix bug

--- python/sglang/srt/eplb/expert_location_updater.py
+++ python/sglang/srt/eplb/expert_location_updater.py
@@ -53,7 +53,7 @@ class ExpertLocationUpdater:
             torch.cuda.empty_cache()
 
         old_expert_location_metadata = get_global_expert_location_metadata()
-        if rank >= global_server_args_dict["num_external_rank"]
+        if rank >= global_server_args_dict["num_external_rank"]:
             _update_expert_weights(
                 routed_experts_weights_of_layer=routed_experts_weights_of_layer,
                 old_expert_location_metadata=old_expert_location_metadata,
