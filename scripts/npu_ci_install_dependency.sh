0e90dbfeff7b9bd0498085ffb961c6fcca529296
qmkakaxi <wjh1594260677@qq.com>
2025-08-07 09:57:54 +0800

    clean code

--- scripts/npu_ci_install_dependency.sh
+++ scripts/npu_ci_install_dependency.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
-CACHING_URL="cache-service.nginx-pypi-cache.svc.cluster.local"
-PIP_INSTALL="pip install --no-cache-dir"
-
-
-# Update apt & pip sources
-sed -Ei "s@(ports|archive).ubuntu.com@${CACHING_URL}:8081@g" /etc/apt/sources.list
-pip config set global.index-url http://${CACHING_URL}/pypi/simple
-pip config set global.trusted-host ${CACHING_URL}
-
-
 # Install the required dependencies in CI.
-apt update -y && apt install -y \
-    build-essential \
-    cmake \
-    wget \
-    curl \
-    net-tools \
-    zlib1g-dev \
-    lld \
-    clang \
-    locales \
-    ccache \
-    ca-certificates
-update-ca-certificates
-python3 -m ${PIP_INSTALL} --upgrade pip
+sed -Ei 's@(ports|archive).ubuntu.com@cache-service.nginx-pypi-cache.svc.cluster.local:8081@g' /etc/apt/sources.list
+apt update -y
+apt install -y build-essential cmake python3-pip python3-dev wget net-tools zlib1g-dev lld clang software-properties-common curl
 
+
+pip config set global.index-url http://cache-service.nginx-pypi-cache.svc.cluster.local/pypi/simple
+pip config set global.trusted-host cache-service.nginx-pypi-cache.svc.cluster.local
+
+python3 -m pip install --upgrade pip --no-cache-dir
+pip uninstall sgl-kernel -y || true
 
 ### Download MemFabricV2
 MF_WHL_NAME="mf_adapter-1.0.0-cp311-cp311-linux_aarch64.whl"
 MEMFABRIC_URL="https://sglang-ascend.obs.cn-east-3.myhuaweicloud.com:443/sglang/${MF_WHL_NAME}"
-wget "${MEMFABRIC_URL}" && ${PIP_INSTALL} "./${MF_WHL_NAME}"
+wget "${MEMFABRIC_URL}" && pip install "./${MF_WHL_NAME}"
 
 
 ### Install vLLM
 VLLM_TAG=v0.8.5
 git clone --depth 1 https://github.com/vllm-project/vllm.git --branch $VLLM_TAG
-(cd vllm && VLLM_TARGET_DEVICE="empty" ${PIP_INSTALL} -v -e .)
+(cd vllm && VLLM_TARGET_DEVICE="empty" pip install -v -e .)
 
 
 ### Install PyTorch and PTA
@@ -50,12 +35,11 @@ pip install ./torch_npu-2.6.0-cp311-cp311-manylinux_2_28_aarch64.whl
 
 ### Install Triton-Ascend
 TRITON_ASCEND_VERSION=3.2.0rc2
-${PIP_INSTALL} attrs==24.2.0 numpy==1.26.4 scipy==1.13.1 decorator==5.1.1 psutil==6.0.0 pytest==8.3.2 pytest-xdist==3.6.1 pyyaml pybind11
-${PIP_INSTALL} triton-ascend==$TRITON_ASCEND_VERSION
+pip install attrs==24.2.0 numpy==1.26.4 scipy==1.13.1 decorator==5.1.1 psutil==6.0.0 pytest==8.3.2 pytest-xdist==3.6.1 pyyaml pybind11 --no-cache-dir
+pip install triton-ascend==$TRITON_ASCEND_VERSION --no-cache-dir
 
 
-### Install SGLang
-${PIP_INSTALL} -v -e "python[srt_npu]"
+pip install -e "python[srt_npu]" --no-cache-dir
 
 
 ### Modify PyTorch TODO: to be removed later
