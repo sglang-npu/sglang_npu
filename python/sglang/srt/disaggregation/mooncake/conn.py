93c28056f7390f5cf8f8de490d860b9b698060c2
ichaoren <fengjian5500@163.com>
2025-08-08 16:52:25 +0800

    log

--- python/sglang/srt/disaggregation/mooncake/conn.py
+++ python/sglang/srt/disaggregation/mooncake/conn.py
@@ -278,6 +278,7 @@ class MooncakeKVManager(BaseKVManager):
         prefill_kv_blocks, dst_kv_blocks = group_concurrent_contiguous(
             prefill_kv_indices, dst_kv_indices
         )
+        logger.info(f"send_kvcache {prefill_kv_blocks=} {dst_kv_blocks=}")
 
         num_layers = len(self.kv_args.kv_data_ptrs)
         layers_params = [
