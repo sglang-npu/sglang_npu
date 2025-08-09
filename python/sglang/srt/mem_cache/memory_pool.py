79571b49d67c3170c2598d9db826114dd175e39d
ichaoren <fengjian5500@163.com>
2025-08-09 09:28:29 +0800

    log

--- python/sglang/srt/mem_cache/memory_pool.py
+++ python/sglang/srt/mem_cache/memory_pool.py
@@ -1038,7 +1038,7 @@ class AscendMLAPagedTokenToKVPool(MLATokenToKVPool):
         layer_id = layer.layer_id
         if layer_id == 0:
             sums = self.kv_buffer[0].sum(dim=(1,2))
-            logger.info(f"set_kv_buffer before {sums[:13]=} {self.kv_buffer[0].sum()=} {self.kv_buffer[0].shape=}")
+            logger.info(f"set_kv_buffer before {sums[:20]=} {self.kv_buffer[0].sum()=} {self.kv_buffer[0].shape=}")
 
         if cache_k.dtype != self.dtype:
             cache_k = cache_k.to(self.dtype)
@@ -1057,7 +1057,7 @@ class AscendMLAPagedTokenToKVPool(MLATokenToKVPool):
         )
         if layer_id == 0:
             sums = self.kv_buffer[0].sum(dim=(1,2))
-            logger.info(f"set_kv_buffer after {sums[:13]=} {self.kv_buffer[0].sum()=} {self.kv_buffer[0].shape=}")
+            logger.info(f"set_kv_buffer after {sums[:20]=} {self.kv_buffer[0].sum()=} {self.kv_buffer[0].shape=}")
 
 
 class DoubleSparseTokenToKVPool(KVCache):
