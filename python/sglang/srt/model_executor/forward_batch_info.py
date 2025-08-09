a145828065386191d6363130e24f15fe30fc9939
todobe <todobe@qq.com>
2025-08-09 14:01:52 +0800

    fix

--- python/sglang/srt/model_executor/forward_batch_info.py
+++ python/sglang/srt/model_executor/forward_batch_info.py
@@ -951,8 +951,8 @@ def compute_position_torch(
             [
                 torch.cat(
                     [
-                        torch.arange(cp_rank * (prefix_len + extend_len) // 2, (cp_rank + 1) * (prefix_len + extend_len) //2, device=extend_prefix_len.device),
-                        torch.arange(sec_rank * (prefix_len + extend_len) // 2, (sec_rank + 1) * (prefix_len + extend_len) //2, device=extend_prefix_len.device)
+                        torch.arange(cp_rank * (prefix_len + extend_len) // 2, (cp_rank + 1) * (prefix_len + extend_len) //2, device=extend_prefix_lens.device),
+                        torch.arange(sec_rank * (prefix_len + extend_len) // 2, (sec_rank + 1) * (prefix_len + extend_len) //2, device=extend_prefix_lens.device)
                     ],
                     axis=0,
                 )
