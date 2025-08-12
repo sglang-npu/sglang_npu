19369b7b82c069ee0e8e7f4dd7e04417d4d7a6e9
qmkakaxi <wjh1594260677@qq.com>
2025-08-12 16:44:31 +0800

    clean code

--- python/sglang/srt/eplb/expert_location_updater.py
+++ python/sglang/srt/eplb/expert_location_updater.py
@@ -145,15 +145,18 @@ def _update_expert_weights_raw(
     world_size = torch.distributed.get_world_size()
     num_local_physical_experts = old_expert_location_metadata.num_local_physical_experts
     num_gpu_per_node = world_size // nnodes
-
+    moe_shared_expert_rank_num = global_server_args_dict[
+        "moe_shared_expert_rank_num"
+    ]
+    external_phys = moe_shared_expert_rank_num * num_local_physical_experts
     for layer_id in update_layer_ids:
         update_expert_weights_single_layer(
             routed_experts_weights=routed_experts_weights_of_layer[layer_id],
             temp_buffers=temp_buffers,
-            old_physical_to_logical_map=old_expert_location_metadata.physical_to_logical_map_cpu[
+            old_physical_to_logical_map=[-1]*external_phys+old_expert_location_metadata.physical_to_logical_map_cpu[
                 layer_id
             ].tolist(),
-            new_physical_to_logical_map=new_expert_location_metadata.physical_to_logical_map_cpu[
+            new_physical_to_logical_map=[-1]*external_physnew_expert_location_metadata.physical_to_logical_map_cpu[
                 layer_id
             ].tolist(),
             num_local_physical_experts=num_local_physical_experts,
@@ -332,9 +335,6 @@ def update_expert_weights_single_layer(
         src_rank: int,
         dst_expert_location: int,
     ):
-        moe_shared_expert_rank_num = global_server_args_dict[
-            "moe_shared_expert_rank_num"
-        ]
         p2p_op_infos.append(
             (
                 logical_expert_id,
@@ -342,7 +342,7 @@ def update_expert_weights_single_layer(
                     P2POp(
                         op=torch.distributed.irecv,
                         tensor=_get_tensor(temp_buffers, i, dst_expert_location),
-                        peer=src_rank+moe_shared_expert_rank_num,
+                        peer=src_rank,
                     )
                     for i in range(num_tensors)
                 ],
@@ -382,11 +382,7 @@ def update_expert_weights_single_layer(
             output_logs.append(
                 f"create_isend_ops_of_logical_expert_id {logical_expert_id=} {src_expert_location=} {same_node_dst_ranks=} {cross_node_dst_ranks=}"
             )
-
-        moe_shared_expert_rank_num = global_server_args_dict[
-            "moe_shared_expert_rank_num"
-        ]
-
+            
         p2p_op_infos.append(
             (
                 logical_expert_id,
@@ -396,7 +392,7 @@ def update_expert_weights_single_layer(
                         tensor=_get_tensor(
                             routed_experts_weights, i, src_expert_location
                         ),
-                        peer=dst_rank+moe_shared_expert_rank_num,
+                        peer=dst_rank,
                     )
                     for dst_rank in all_dst_ranks
                     for i in range(num_tensors)
