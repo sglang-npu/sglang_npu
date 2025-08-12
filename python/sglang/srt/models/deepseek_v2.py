369a815b23bcffa545094b4857a0f47e7ff8d697
qmkakaxi <wjh1594260677@qq.com>
2025-08-12 21:16:59 +0800

    test

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -607,10 +607,11 @@ class DeepseekV2MoE(nn.Module):
         self, hidden_states: torch.Tensor, forward_batch: ForwardBatch
     ) -> torch.Tensor:
         shared_output = None
+        test = True
         if hidden_states.shape[0] > 0:
             # router_logits: (num_tokens, n_experts)
             router_logits = self.gate(hidden_states)
-            if self.moe_shared_expert_rank_num == 0:
+            if self.moe_shared_expert_rank_num == 0 or test = True:
                 shared_output = self._forward_shared_experts(hidden_states)
             else:
                 shared_output = None
